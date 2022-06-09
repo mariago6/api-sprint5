d0ecce90e52be5d5805c0bdaa4a4f6779e5294d2
Maria Go <merigon_93@hotmail.com>
2022-06-09 10:53:08 +0200

    EXERCISE_5_LEVEL2

--- script.js
+++ script.js
@@ -1,19 +1,42 @@
-const API_URL = 'https://icanhazdadjoke.com/'; 
+const API_JOKES = 'https://icanhazdadjoke.com/'; 
 const API_WEATHER = 'https://api.openweathermap.org/data/2.5/weather?q=Barcelona,ES&appid=0c219334560571ca1d8636a5b846e5d8'; 
+const API_CHUCKNORRIS = 'https://api.chucknorris.io/jokes/random'; 
 let currentJoke; 
+let currentChuckNorrisJoke; 
 let currentWeather; 
 let currentTemperature; 
 let reportJokes = [];
 let isFirstJoke = true;
 getWeather(); 
+let randomNumber = true; 
+
+function printJokes() {
+  if (randomNumber) {
+    getUser();
+    randomNumber = false;
+    return; 
+  } else {
+    getJokesChuckNorris();
+    randomNumber = true; 
+    return; 
+  }
+}
 
 function getUser() {
-  fetch(API_URL, {
+  fetch(API_JOKES, {
     headers: { Accept: 'application/json' }
   })
   .then(response => response.json())
   .then(data => currentJoke = data.joke)
   .then(() => document.getElementById('joke').innerHTML = currentJoke);
+
+}
+
+function getJokesChuckNorris() {
+  fetch(API_CHUCKNORRIS)
+    .then(response => response.json())
+    .then(data => currentChuckNorrisJoke = data.value)
+    .then(() => document.getElementById('joke').innerHTML = currentChuckNorrisJoke);   
 }
 
 function getWeather() {
